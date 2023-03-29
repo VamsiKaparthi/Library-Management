d5bd05c29437dc3cfda740cd94c096197187885d
Vamsi <vk784@snu.edu.in>
2023-03-29 12:21:42 +0530

    Open form when click on add book functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let add = document.getElementById("add");
 add.addEventListener("click",()=>{
-    prompt("hi");
+    document.getElementById("form").style.display = "flex";
+    console.log("hi");
 })
