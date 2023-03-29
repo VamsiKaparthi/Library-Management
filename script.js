4ffa389a10c32da06709bebc41c4427bf865733d
Vamsi <vk784@snu.edu.in>
2023-03-29 18:19:23 +0530

    Add functionality to Get data from form fields after clicking submit button

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ add.addEventListener("click",()=>{
     document.getElementById("form").style.display = "flex";
     console.log("hi");
 })
+let submit = document.getElementById("submit")
+submit.addEventListener("click",()=>{
+    console.log(document.getElementById("Book-name").value)
+    console.log(document.getElementById("Author").value)
+    console.log(document.getElementById("pages").value)
+})
