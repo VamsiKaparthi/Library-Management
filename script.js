c8d38144e72637fb18809f75ebc03377518cc713
Vamsi <vk784@snu.edu.in>
2023-03-29 18:35:13 +0530

    Add Book and addBookToLibrary functions

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ add.addEventListener("click",()=>{
     document.getElementById("form").style.display = "flex";
     console.log("hi");
 })
+let myLibrary=[];
+function Book(book,author,pages,check){
+    this.bookName=book;
+    this.authorName=author;
+    this.pagesNo=pages;
+    this.checkBox=check;
+}
+function addBookToLibrary(book){
+    myLibrary.push(book)
+}
+
 let submit = document.getElementById("submit")
 submit.addEventListener("click",()=>{
-    console.log(document.getElementById("Book-name").value)
-    console.log(document.getElementById("Author").value)
-    console.log(document.getElementById("pages").value)
+    
 })
