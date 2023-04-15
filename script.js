89ebec46725838982d0a760598e76313b877394f
Vamsi <vk784@snu.edu.in>
2023-04-15 16:07:24 +0530

    Add prefentDefault() to submit button to recieve data from form

--- script.js
+++ script.js
@@ -15,6 +15,23 @@ function addBookToLibrary(book){
 }
 
 let submit = document.getElementById("submit")
-submit.addEventListener("click",()=>{
-    
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
+    let bookName = document.getElementById("Book-name");
+    let author = document.getElementById("Author");
+    let no= document.getElementById("pages");
+    if(bookName.value==""||author.value==""||no.value==""){
+        alert("Ensure you fill in all fields");
+    }
+    else{
+        let book = new Book(document.getElementById("Book-name").value,document.getElementById("Author").value,document.getElementById("pages").value,document.getElementById("read").value);
+        console.log(document.getElementById("Book-name").value)
+        addBookToLibrary(book);
+        bookName.value=""
+        author.value=""
+        no.value=""
+        document.getElementById("form").style.display="none";
+        console.log(myLibrary)
+    }
 })
+console.log(myLibrary)
